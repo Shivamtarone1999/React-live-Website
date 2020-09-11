d9649d18c03e9f9270c7b61753cbac68d9b52036
Shivam Tarone <shivamtarone07@gmail.com>
2020-09-11 10:30:45 +0530

    Updates

---
+++ precache-manifest.f6f3dfdf752a67a868a37476faa18274.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6043bd3777efb4c69d7169d46e4f4f",
+    "url": "/React-live-Website/index.html"
+  },
+  {
+    "revision": "7b2e047993f83d8febe6",
+    "url": "/React-live-Website/static/css/main.9669d848.chunk.css"
+  },
+  {
+    "revision": "e27be403363f8e748f4a",
+    "url": "/React-live-Website/static/js/2.bc44ed59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-live-Website/static/js/2.bc44ed59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2e047993f83d8febe6",
+    "url": "/React-live-Website/static/js/main.075f6f2d.chunk.js"
+  },
+  {
+    "revision": "8279ecbee956e38bcdce",
+    "url": "/React-live-Website/static/js/runtime-main.c58916c8.js"
+  },
+  {
+    "revision": "e5e26347a7e0f1d29d0eaeed3e27b0a3",
+    "url": "/React-live-Website/static/media/about.e5e26347.svg"
+  },
+  {
+    "revision": "3b2992312eb4b1c63a63c351dfaf3355",
+    "url": "/React-live-Website/static/media/app.3b299231.png"
+  },
+  {
+    "revision": "01773a681de5dbdb93ec119b2995557b",
+    "url": "/React-live-Website/static/media/danalysis.01773a68.jpg"
+  },
+  {
+    "revision": "7102a1cd7551cfadfd6bff5cdb913496",
+    "url": "/React-live-Website/static/media/digital.7102a1cd.jpg"
+  },
+  {
+    "revision": "980291e0181f1f42693fa73db0dcd2e5",
+    "url": "/React-live-Website/static/media/img.980291e0.svg"
+  },
+  {
+    "revision": "fde97e2e58a6bb50bab4542cebfe8cf1",
+    "url": "/React-live-Website/static/media/seo.fde97e2e.jpg"
+  },
+  {
+    "revision": "d15efd9b9f89d09485863314171edf5e",
+    "url": "/React-live-Website/static/media/software.d15efd9b.jpg"
+  },
+  {
+    "revision": "d3fe01b574ab4fe2821334079816a1bd",
+    "url": "/React-live-Website/static/media/web.d3fe01b5.png"
+  }
+]);
